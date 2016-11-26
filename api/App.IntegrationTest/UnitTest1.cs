c439c0bd70604a53f8c37478130f4a857802fbcf
MyERP <tu.tran@yahoo.com>
2016-11-26 17:00:08 +0700

    Update unittest base class

---
+++ api/App.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace App.IntegrationTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
