ffd8ab0894283737b732661d3a04b99f157a5c95
ORIENTSOFTWARE\tttu <tu.tran@orientsoftware.net>
2016-11-29 15:24:46 +0700

    Add integration test for update permission

--- api/App.IntegrationTest/Security/Permission/UpdatePermission.cs
+++ api/App.IntegrationTest/Security/Permission/UpdatePermission.cs
@@ -20,14 +20,14 @@
         public void Security_Permission_UpdatePermission_ShouldBeSuccess_WithValidRequest()
         {
             UpdatePermissionRequest request = new UpdatePermissionRequest(this.createdPermissionResponse.Id, "new updated name" + Guid.NewGuid(), "New updated Key " + Guid.NewGuid(), "desc");
-            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(String.Format(this.BaseUrl, request.Id), request);
+            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(string.Format(this.BaseUrl, request.Id), request);
             Assert.IsTrue(response.Errors.Count == 0);
         }
 
         private CreatePermissionResponse CreateNewPermission()
         {
             CreatePermissionRequest request = new CreatePermissionRequest("update permission name" + Guid.NewGuid(), "update permission key" + Guid.NewGuid(), "desc");
-            IResponseData<CreatePermissionResponse> response = this.Connector.Post<CreatePermissionRequest, CreatePermissionResponse>(String.Format(this.BaseUrl, ""), request);
+            IResponseData<CreatePermissionResponse> response = this.Connector.Post<CreatePermissionRequest, CreatePermissionResponse>(string.Format(this.BaseUrl, string.Empty), request);
             return response.Data;
         }
 
@@ -35,7 +35,7 @@
         public void Security_Permission_UpdatePermission_ShouldThrowException_WithEmptyNameAndKey()
         {
             UpdatePermissionRequest request = new UpdatePermissionRequest(this.createdPermissionResponse.Id, string.Empty, string.Empty, "desc");
-            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(String.Format(this.BaseUrl, request.Id), request);
+            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(string.Format(this.BaseUrl, request.Id), request);
             Assert.IsTrue(response.Errors.Count > 0);
             Assert.IsTrue(response.Errors.Any(item => item.Key == "security.addPermission.validation.nameIsRequire"));
             Assert.IsTrue(response.Errors.Any(item => item.Key == "security.addPermission.validation.keyIsRequire"));
@@ -45,7 +45,7 @@
         public void Security_Permission_CreatePermission_ShouldThrowException_WithNotExistedId()
         {
             UpdatePermissionRequest request = new UpdatePermissionRequest(Guid.NewGuid(), "new updated name" + Guid.NewGuid(), "New updated Key " + Guid.NewGuid(), "desc");
-            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(String.Format(this.BaseUrl, request.Id), request);
+            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(string.Format(this.BaseUrl, request.Id), request);
             Assert.IsTrue(response.Errors.Count > 0);
             Assert.IsTrue(response.Errors.Any(item => item.Key == "security.addPermission.validation.invalidId"));
         }
@@ -54,7 +54,7 @@
         public void Security_Permission_CreatePermission_ShouldThrowException_WithEmptyId()
         {
             UpdatePermissionRequest request = new UpdatePermissionRequest(Guid.Empty, "new updated name" + Guid.NewGuid(), "New updated Key " + Guid.NewGuid(), "desc");
-            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(String.Format(this.BaseUrl, request.Id), request);
+            IResponseData<string> response = this.Connector.Put<UpdatePermissionRequest, string>(string.Format(this.BaseUrl, request.Id), request);
             Assert.IsTrue(response.Errors.Count > 0);
             Assert.IsTrue(response.Errors.Any(item => item.Key == "common.validation.invalidRequest"));
         }
