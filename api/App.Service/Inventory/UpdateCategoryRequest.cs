c409ebc9ea5e6695f5f1d537e09767528c703870
minhhuyen93 <nmhuyen.hcmus@gmail.com>
2016-12-06 20:40:15 +0700

    Implement validation edit server

--- api/App.Service/Inventory/UpdateCategoryRequest.cs
+++ api/App.Service/Inventory/UpdateCategoryRequest.cs
@@ -1,10 +1,16 @@
 ﻿namespace App.Service.Inventory
 {
     using System;
+    using App.Common;
+    using App.Common.Validation.Attribute;
+
     public class UpdateCategoryRequest
     {
         public Guid Id { get; set; }
+        [ValueInRange("inventory.addOrUpdateCategory.validation.fieldTooLong",0, FormValidationRules.MaxNameLength)]
+        [Required("inventory.addOrUpdateCategory.validation.nameRequired")]
         public string Name { get; set; }
+        [ValueInRange("inventory.addOrUpdateCategory.validation.fieldTooLong", 0 , FormValidationRules.MaxDescriptionLength)]
         public string Description { get; set; }
     }
 }
